799b700e4714d68636eec5407e90906cb24a32e2
Emma <emmagray183@gmail.com>
2019-11-11 16:40:57 +0000

    book info method

--- library/library.rb
+++ library/library.rb
@@ -1,11 +1,18 @@
 class Library
 
-  attr_accessor :books 
+  attr_accessor :books
 
 def initialize(input_books)
   @books = input_books
 end
 
+def book_info(book_title)
+  for book in @books
+    if book[:title] == book_title
+      return book
+    end
+  end
+end
 
 
 
--- library/library_specs.rb
+++ library/library_specs.rb
@@ -4,30 +4,32 @@ require_relative('library')
 
 class TestLibrary < MiniTest::Test
 
-  def test_get_books
-    glasgow_library = Library.new([
-      {
-    title: "lord_of_the_rings",
-    rental_details: {
-     student_name: "Jeff",
-     date: "01/12/16"
-    }
-  },
-  {
-    title: "50_shades_of_grey",
-    rental_details: {
-     student_name: "Doris",
-     date: "03/03/16"
-    }
-  },
-  {
-    title: "the_handmaids_tale",
-    rental_details: {
-     student_name: "Suzie",
-     date: "01/10/16"
-    }
+  @@books = [
+    {
+  title: "lord_of_the_rings",
+  rental_details: {
+   student_name: "Jeff",
+   date: "01/12/16"
   }
-    ])
+},
+{
+  title: "50_shades_of_grey",
+  rental_details: {
+   student_name: "Doris",
+   date: "03/03/16"
+  }
+},
+{
+  title: "the_handmaids_tale",
+  rental_details: {
+   student_name: "Suzie",
+   date: "01/10/16"
+  }
+}
+  ]
+
+  def test_get_books
+    glasgow_library = Library.new(@@books)
      assert_equal([
        {
      title: "lord_of_the_rings",
@@ -52,6 +54,21 @@ class TestLibrary < MiniTest::Test
    }
      ], glasgow_library.books)
   end
+
+  def test_book_info
+    glasgow_library = Library.new(@@books)
+
+    assert_equal(
+      {
+      title: "the_handmaids_tale",
+      rental_details: {
+       student_name: "Suzie",
+       date: "01/10/16" }
+     },
+      glasgow_library.book_info("the_handmaids_tale"))
+  end
+
+
 
 
 
