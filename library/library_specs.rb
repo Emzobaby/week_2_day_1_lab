5ba4424ce0c05ff9459fca29ef5aa1a05d74ea4a
Emma <emmagray183@gmail.com>
2019-11-11 16:22:51 +0000

    tests get books

--- library/library_specs.rb
+++ library/library_specs.rb
@@ -4,29 +4,54 @@ require_relative('library')
 
 class TestLibrary < MiniTest::Test
 
-  @books = [
-    {
-  title: "lord_of_the_rings",
-  rental_details: {
-   student_name: "Jeff",
-   date: "01/12/16"
+  def test_get_books
+    glasgow_library = Library.new([
+      {
+    title: "lord_of_the_rings",
+    rental_details: {
+     student_name: "Jeff",
+     date: "01/12/16"
+    }
+  },
+  {
+    title: "50_shades_of_grey",
+    rental_details: {
+     student_name: "Doris",
+     date: "03/03/16"
+    }
+  },
+  {
+    title: "the_handmaids_tale",
+    rental_details: {
+     student_name: "Suzie",
+     date: "01/10/16"
+    }
   }
-},
-{
-  title: "50_shades_of_grey",
-  rental_details: {
-   student_name: "Doris",
-   date: "03/03/16"
-  }
-},
-{
-  title: "the_handmaids_tale",
-  rental_details: {
-   student_name: "Suzie",
-   date: "01/10/16"
-  }
-}
-  ]
+    ])
+     assert_equal([
+       {
+     title: "lord_of_the_rings",
+     rental_details: {
+      student_name: "Jeff",
+      date: "01/12/16"
+     }
+   },
+   {
+     title: "50_shades_of_grey",
+     rental_details: {
+      student_name: "Doris",
+      date: "03/03/16"
+     }
+   },
+   {
+     title: "the_handmaids_tale",
+     rental_details: {
+      student_name: "Suzie",
+      date: "01/10/16"
+     }
+   }
+     ], glasgow_library.books)
+  end
 
 
 
