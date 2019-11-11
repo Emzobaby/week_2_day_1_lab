720d2af252ec5a40b1be69f72b3489e83a52a73e
Emma <emmagray183@gmail.com>
2019-11-11 21:17:47 +0000

    refactoring use @@books to fix lengthy writing out of array

--- library/library_specs.rb
+++ library/library_specs.rb
@@ -30,29 +30,7 @@ class TestLibrary < MiniTest::Test
 
   def test_get_books
     glasgow_library = Library.new(@@books)
-     assert_equal([
-       {
-     title: "lord_of_the_rings",
-     rental_details: {
-      student_name: "Jeff",
-      date: "01/12/16"
-     }
-   },
-   {
-     title: "50_shades_of_grey",
-     rental_details: {
-      student_name: "Doris",
-      date: "03/03/16"
-     }
-   },
-   {
-     title: "the_handmaids_tale",
-     rental_details: {
-      student_name: "Suzie",
-      date: "01/10/16"
-     }
-   }
-     ], glasgow_library.books)
+     assert_equal(@@books, glasgow_library.books)
   end
 
   def test_book_info_found
@@ -87,48 +65,19 @@ class TestLibrary < MiniTest::Test
     assert_equal("Da Vinci Code not found.", glasgow_library.get_rental_details("Da Vinci Code"))
   end
 
-  # def test_add_new_book
-  #     glasgow_library = Library.new(@@books)
-  #     glasgow_library.add_new_book("the_jungle_book")
-  #     assert_equal([
-  #       {
-  #     title: "lord_of_the_rings",
-  #     rental_details: {
-  #      student_name: "Jeff",
-  #      date: "01/12/16"
-  #     }
-  #   },
-  #   {
-  #     title: "50_shades_of_grey",
-  #     rental_details: {
-  #      student_name: "Doris",
-  #      date: "03/03/16"
-  #     }
-  #   },
-  #   {
-  #     title: "the_handmaids_tale",
-  #     rental_details: {
-  #      student_name: "Suzie",
-  #      date: "01/10/16"
-  #     }
-  #   },
-  #   {
-  #     title: "the_jungle_book",
-  #     rental_details: {
-  #      student_name: "",
-  #      date: ""
-  #     }
-  #   }
-  #     ], glasgow_library.books)
-  # end
-  #
-  # # def test_change_rental_details
-  # #   glasgow_library = Library.new(@@books)
-  # #   glasgow_library.change_rental_details("50_shades_of_grey", "Paddy", "01/12/19")
-  # #   assert_equal({
-  # #    student_name: "Paddy",
-  # #    date: "01/12/19"
-  # #   }, glasgow_library.books[1][:rental_details])
-  # # end
+  def test_add_new_book
+      glasgow_library = Library.new(@@books)
+      glasgow_library.add_new_book("the_jungle_book")
+      assert_equal(@@books, glasgow_library.books)
+  end
+
+  def test_change_rental_details
+    glasgow_library = Library.new(@@books)
+    glasgow_library.change_rental_details("50_shades_of_grey", "Paddy", "01/12/19")
+    assert_equal({
+     student_name: "Paddy",
+     date: "01/12/19"
+    }, glasgow_library.books[1][:rental_details])
+  end
 
 end
