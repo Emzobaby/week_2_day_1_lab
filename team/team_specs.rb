2cd474a8b5347dcc3e4abb089a4701e14409d873
Emma <emmagray183@gmail.com>
2019-11-11 15:44:20 +0000

    create class and test getters

--- team/team_specs.rb
+++ team/team_specs.rb
@@ -8,6 +8,24 @@ def test_can_create_team
   st_mirren_fc = Team.new("St. Mirren", ["Barry Lavety", "Kenny McDowall", "Campbell Money"], "Tony Fitzpatrick")
 end
 
+def test_team_name
+  st_mirren_fc = Team.new("St. Mirren", ["Barry Lavety", "Kenny McDowall", "Campbell Money"], "Tony Fitzpatrick")
+  assert_equal("St. Mirren", st_mirren_fc.team_name)
+end
+
+def test_get_players
+  st_mirren_fc = Team.new("St. Mirren", ["Barry Lavety", "Kenny McDowall", "Campbell Money"], "Tony Fitzpatrick")
+  assert_equal(["Barry Lavety", "Kenny McDowall", "Campbell Money"], st_mirren_fc.players)
+end
+
+def test_get_coach
+  st_mirren_fc = Team.new("St. Mirren", ["Barry Lavety", "Kenny McDowall", "Campbell Money"], "Tony Fitzpatrick")
+  assert_equal("Tony Fitzpatrick", st_mirren_fc.coach)
+end
+
+
+
+
 
 
 end
